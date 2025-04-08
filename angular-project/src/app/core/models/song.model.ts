c4d48d393815063d670a4159290ab8ccf3f86db0
maggie serrano <https://github.com/magser110/October2024.git>
2025-04-07 20:00:07 -0400

    set up song service and model

--- angular-project/src/app/core/models/song.model.ts
+++ angular-project/src/app/core/models/song.model.ts
@@ -1,1 +1,8 @@
 //song inferface
+export interface Song {
+  title: string;
+  artist: string;
+  album: string;
+  genre: string;
+  year: number;
+}
--- angular-project/src/app/core/services/song.service.ts
+++ angular-project/src/app/core/services/song.service.ts
@@ -1,1 +1,60 @@
 //service to manage songs (crud???)
+
+import { signal } from "@angular/core";
+import { Song } from "../models/song.model";
+
+
+export class SongService {
+  private songs = signal<Song[]>([
+    {
+      title: 'Lovers In a Past linkedSignalSetFn',
+      artist: 'Calvin Harris',
+      album: 'Lovers In A Past Life (Remixes)',
+      genre: 'Dance/Electonic',
+      year: 2024
+    },
+    {
+      title: 'VeLDÁ',
+      artist: 'Bad Bunny, Omar Courtz, Dei V',
+      album: 'DeBÍ TiRAR MáS FOToS',
+      genre: 'Reggaeton',
+      year: 2025
+    },
+    {
+      title: 'Closer',
+      artist: 'RM, Paul Blanco, Mahalia',
+      album: 'Indigo',
+      genre: 'Rhythm and blues, K-pop',
+      year: 2022
+    },
+    {
+      title: 'Alors on danse',
+      artist: 'Stromae',
+      album: 'Cheese',
+      genre: 'French Indie, Dance/Electronic',
+      year: 2010
+    },
+    {
+      title: '＊~アスタリスク~',
+      artist: 'ORANGE RANGE',
+      album: '＊~アスタリスク~',
+      genre: 'J-pop',
+      year: 2005
+    }
+  ]);
+
+  getSongs(){
+    return this.songs.asReadonly();
+  }
+
+  addSong(t: string, art: string, alb: string, g: string, y: number){
+    this.songs.update(s => [...s, {title: t, artist: art, album: alb, genre: g, year: y}])
+    //s is song and i am updating the Songs list to include new song
+  }
+
+  removeSong(songName: string){
+    this.songs.update(songList => songList.filter(s => s.title !== songName))
+    //songList is the entire song list/array, s is the individual song in the filter method
+
+  }
+}
