75334ede67ab1ead736c1c6e6add44964c703f9e
maggie serrano <https://github.com/magser110/October2024.git>
2025-04-20 21:24:30 -0400

    generate new Id all good

--- angular-project/src/app/features/song-form/song-form.component.ts
+++ angular-project/src/app/features/song-form/song-form.component.ts
@@ -16,52 +16,66 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './song-form.component.html',
   styleUrl: './song-form.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SongFormComponent implements AfterViewInit {
   private songService = inject(SongService);
 
-
-
   private songForm =
     viewChild.required<ElementRef<HTMLFormElement>>('songForm');
 
-  isEditing = this.songService.isEditing
-  songToEdit = this.songService.songToEdit
+  isEditing = this.songService.isEditing;
+  songToEdit = this.songService.songToEdit;
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit called: form element available');
   }
 
-  generateNewId(): number{
+  generateNewId(): number {
     let newId = this.songService.getSongs()(); //second () used to unwrap signal and actually get song[]
-    let maxId = Math.max(...newId.map(s => s.id));
+    let maxId = Math.max(...newId.map((s) => s.id));
     return maxId + 1;
   }
 
-  songFormHandler(id: number, t: string, art: string, alb: string, g: string, y: number) {
+  scrollToForm() {
+    this.songForm().nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  songFormHandler(
+    id: number,
+    t: string,
+    art: string,
+    alb: string,
+    g: string,
+    y: number
+  ) {
     if (!this.isEditing()) {
       let newId = id || this.generateNewId();
 
       this.songService.addSong(newId, t, art, alb, g, y);
       console.log(t, art, alb, g, y);
     } else {
-      this.songService.updateSong(id!, t, art, alb, g, y); //id songToEdit.id
-    };
+      this.songService.updateSong(id!, t, art, alb, g, y);
+    }
 
     this.songService.isEditing.set(false);
     this.songService.songToEdit.set(null);
-
     this.resetSongFormHandler();
+
+    setTimeout(() => {
+      this.scrollToForm();
+    }, 100);
   }
 
   resetSongFormHandler() {
     this.songForm().nativeElement.reset();
   }
 
-  get deBugOutput(){
+  get deBugOutput() {
     console.log('[SongFormComponent] generated');
     return '';
   }
-
 }
